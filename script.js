9df4fe001f59d6beb1d38029e9008f6c6e316383
Ridoy-vai <mdsahariyarridoy@gmail.com>
2026-02-23 15:48:25 +0600

    hover and count update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let rejectJobsCount = document.getElementById("reject-jobs-count");
 let mainBox = document.getElementById("main-job-box");
 let filterSection = document.getElementById("filter-section");
 let interviewAndRejictCount = document.getElementById("interview-And-Rejict-Count");
-console.log(interviewAndRejictCount.innerText)
+// console.log(interViewJobsCount.innerText)
 
 // All Buttons ID Connect
 let allJobButton = document.getElementById("job-find-All-button");
@@ -24,20 +24,20 @@ function updateCounts() {
 // All Job Inertext Change
 function totalJOb() {
     document.getElementById("all-jobs-child").innerText = mainBox.children.length;
-    interviewAndRejictCount.innerText = mainBox.children.length;
+    // interviewAndRejictCount.innerText = mainBox.children.length + " of ";
 }
 
 // Interview List Count Update
 function jobFindInterviewButton() {
     if (interviewAndRejictCount) {
-        interviewAndRejictCount.innerText = interViewList.length;
+        interviewAndRejictCount.innerText = interViewList.length + " of ";
     }
 }
 
 // Rejict List Count Update
 function jobFindRejectButton() {
     if (interviewAndRejictCount) {
-        interviewAndRejictCount.innerText = rejectsList.length;
+        interviewAndRejictCount.innerText = rejectsList.length + " of ";
     }
 }
 
@@ -68,15 +68,18 @@ function toggleStyle(id) {
     if (id === 'job-find-interview-button') {
         mainBox.classList.add('hidden');
         filterSection.classList.remove('hidden');
+        interviewAndRejictCount.classList.remove("hidden") 
         renderApprove();
         jobFindInterviewButton(); 
     } else if (id === 'job-find-All-button') {
         mainBox.classList.remove('hidden');
+        interviewAndRejictCount.classList.add("hidden")
         filterSection.classList.add('hidden');
         totalJOb();
     } else if (id === 'job-find-reject-button') {
         mainBox.classList.add('hidden');
         filterSection.classList.remove('hidden');
+        interviewAndRejictCount.classList.remove("hidden")
         renderRejected();
         jobFindRejectButton(); 
     }
@@ -106,10 +109,14 @@ document.addEventListener('click', function (event) {
     const isRejectedBtn = event.target.classList.contains("rejected-btn");
     const isDeleteBtn = event.target.classList.contains("delete-btn");
 
-    if (!isInterviewBtn && !isRejectedBtn && !isDeleteBtn) return;
+    if (!isInterviewBtn && !isRejectedBtn && !isDeleteBtn) {
+        return;
+    }
 
     const parentNode = event.target.closest('.job-card');
-    if (!parentNode) return;
+    if (!parentNode) {
+        return;
+    }
 
     const jobName = parentNode.querySelector('.job_title').innerText;
 
@@ -139,12 +146,14 @@ document.addEventListener('click', function (event) {
     const jobDetails = parentNode.querySelector('.job_details').innerText;
     const jobData = { jobName, jobSkill, jobSalary, jobDetails };
 
+    // Object push in Interview Array
     if (isInterviewBtn) {
         rejectsList = rejectsList.filter(item => item.jobName !== jobName);
         if (!interViewList.find(item => item.jobName === jobName)) interViewList.push(jobData);
         updateMainBoxUI(jobName, 'interview');
     }
 
+    // Object Push Reject Array
     if (isRejectedBtn) {
         interViewList = interViewList.filter(item => item.jobName !== jobName);
         if (!rejectsList.find(item => item.jobName === jobName)) rejectsList.push(jobData);
@@ -161,23 +170,36 @@ document.addEventListener('click', function (event) {
     updateActiveTabCount(); 
 });
 
-// Blank Page Loadded
+// Blank Interview Page Loadded
 function renderApprove() {
     filterSection.innerHTML = ""; 
     if(interViewList.length === 0){
-        filterSection.innerHTML = `<div class="flex flex-col items-center justify-center p-4 text-center bg-gray-200 rounded-[20px] mt-6">
-            <div class="mb-5">
-                <svg class="w-20 h-24 text-blue-400 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14,2H6C4.89,2 4,2.89 4,4V20C4,21.1 4.89,22 6,22H18C19.1,22 20,21.1 20,20V8L14,2M13,9V3.5L18.5,9H13M16,19H8V17H16V19M16,15H8V13H16V15M13,11H8V9H13V11Z"/>
-                </svg>
-            </div>
-            <h1 class="text-2xl md:text-3xl font-bold text-[#002d5b]">No jobs available</h1>
-            <p class="mt-2 text-gray-500">Check back soon for new job opportunities</p>
-        </div>`;
+        filterSection.innerHTML = `<div class="flex items-center justify-center px-4 mt-10">
+            <div class="flex flex-col items-center justify-center text-center 
+                bg-white shadow-lg rounded-2xl p-10 w-full 
+                border border-gray-100">
+
+                <!-- Icon -->
+                <div class="flex items-center justify-center w-24 h-24 
+                            rounded-full bg-blue-100 mb-6">
+                    <i class="fa-regular fa-file-lines text-5xl text-blue-500"></i>
+                </div>
+
+                <!-- Title -->
+                <h1 class="text-2xl md:text-3xl font-bold text-gray-800">
+                    No Jobs Available
+                </h1>
+
+                <!-- Description -->
+                <p class="mt-3 text-gray-500 text-sm md:text-base">
+                    Check back soon for new job opportunities.
+                </p>
+                </div>
+            </div>`;
         return;
     }
 
-    // Dynamick Data Loaded
+    // Dynamick Interview Data Loaded
     interViewList.forEach(interview => {
         let div = document.createElement('div');
         div.innerHTML = `<div class="job-card" style="margin-top: 20px; padding: 15px; background-color: #E8F5E9; border-radius: 8px; border: 1px solid #4CAF50;">
@@ -202,23 +224,33 @@ function renderApprove() {
     });
 }
 
-// Blank Page Loaded
+// Blank Reject Page Loaded
 function renderRejected() {
     filterSection.innerHTML = ""; 
     if(rejectsList.length === 0){
-        filterSection.innerHTML = `<div class="flex flex-col items-center justify-center p-4 text-center bg-gray-200 rounded-[20px] mt-6">
-            <div class="mb-5">
-                <svg class="w-20 h-24 text-blue-400 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14,2H6C4.89,2 4,2.89 4,4V20C4,21.1 4.89,22 6,22H18C19.1,22 20,21.1 20,20V8L14,2M13,9V3.5L18.5,9H13M16,19H8V17H16V19M16,15H8V13H16V15M13,11H8V9H13V11Z"/>
-                </svg>
-            </div>
-            <h1 class="text-2xl md:text-3xl font-bold text-[#002d5b]">No jobs available</h1>
-            <p class="mt-2 text-gray-500">Check back soon for new job opportunities</p>
-        </div>`;
+        filterSection.innerHTML =`<div class="flex items-center justify-center px-4 mt-10">
+            <div class="flex flex-col items-center justify-center text-center 
+                    bg-white shadow-lg rounded-2xl p-10 w-full 
+                    border border-gray-100">
+                    <!-- Icon -->
+                    <div class="flex items-center justify-center w-24 h-24 
+                                rounded-full bg-blue-100 mb-6">
+                        <i class="fa-regular fa-file-lines text-5xl text-blue-500"></i>
+                    </div>
+                    <!-- Title -->
+                    <h1 class="text-2xl md:text-3xl font-bold text-gray-800">
+                        No Jobs Available
+                    </h1>
+                    <!-- Description -->
+                    <p class="mt-3 text-gray-500 text-sm md:text-base">
+                        Check back soon for new job opportunities.
+                    </p>
+                </div>
+            </div>`;
         return;
     }
 
-    // Dynamic Data Loaded
+    // Dynamic Reject Data Loaded
     rejectsList.forEach(reject => {
         let div = document.createElement('div');
         div.innerHTML = `<div class="job-card" style="margin-top: 20px; padding: 15px; background-color: #FFEBEE; border-radius: 8px; border: 1px solid #F44336;">
